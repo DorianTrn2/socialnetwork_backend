d504931556f7a059ddc04366f43a6037e993a624
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2024-11-05 17:19:37 +0100

    feat: authentification routes and logic

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const path = require('path');
 const session = require("express-session");
 
 const app = express();
-const port = 3000;
+const port = 3001;
+app.use(morgan('dev'));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-// middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }))
 
@@ -21,48 +19,12 @@ app.use(session({
     saveUninitialized: true
 }));
 
-// middleware d'authentification
-function auth(req, res, next) {
-    if (req?.session?.user) {
-        return next();
-    }
-    else {
-        return res.sendStatus(401);
-    }
-}
+const indexRouter = require('./routes/index.js');
+const authRouter = require('./routes/auth.js');
 
-app.get('/', function(req, res) {
-    if(req?.session?.user){
-        res.redirect("/home");
-    }
-    else{
-        res.redirect("/login");
-    }
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
-});
-
-app.get('/login', function(req, res) {
-    console.log("login");
-    res.render("login", {title: "Connexion"});
-});
-
-app.post('/login', function(req, res) {
-
-    // Utilisateur fake ici
-    // TODO : aller chercher l'utilisateur en base de données à partir du login et du (hash du) mot de passe
-    req.session.user = { firstname : "Jean", lastname : "Dupond"};
-    res.redirect("/home");
-});
-
-app.get('/home', auth, function(req, res) {
-    res.render("layout", {title: "Page d'accueil", user : req.session.user});
-});
-
-app.post('/logout', function(req, res) {
-
-    req.session.destroy();
-    res.redirect("/login");
-});
 
 // server start
 app.listen(port, () => {
---
+++ routes/auth.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../model/User.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', async (req, res) => {
+    try {
+        const { login, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username: login, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Registration failed' });
+    }
+});
+
+router.get('/login', (req, res) =>
+    res.render( "login.pug", {title: "Sign up !"}));
+
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch || !user) {
+            return res.status(401).json({ error: 'Authentication failed' });
+        }
+        const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
+            expiresIn: '1h',
+        });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: 'Login failed' });
+    }
+});
+
+router.post('/logout', async(req, res) => {
+    try{
+        res.clearCookie('token');
+        res.status(200).json({ message: 'Logout successful' });
+        res.redirect('/');
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Logout failed' });
+    }
+});
+
+module.exports = router;
