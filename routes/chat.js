1aff5c7e5f997723074c01ad40f16e9cc89002d4
Vazelek <valentinherve60@gmail.com>
2024-11-08 08:53:53 +0100

    add can see chat only if user of chat or admin

--- routes/chat.js
+++ routes/chat.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const chatController = require("../controllers/chatController");
+const {verifyAdminToken, verifyUserHaveAccessToken} = require("../middleware/authMiddleware");
 
 router.get('', chatController.getAllChatsOfUser);
+router.get('/all', verifyAdminToken, chatController.getAllChats);
+router.get('/:chat_id', verifyUserHaveAccessToken, chatController.getChatById);
 
 module.exports = router;
