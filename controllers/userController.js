5b5bb6b1eeff2739af93d86917c1e7222a2d6a61
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2024-11-08 14:25:27 +0100

    feat: user logic (update, profile, getters)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,73 @@
-// TODO
+const userService = require("../services/userService");
+const eventService = require("../services/eventService");
+
+async function getAllUsers(req, res) {
+    try{
+        const users = await userService.getAllUsers();
+        res.status(200).json(users);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to retrieve users' });
+    }
+}
+
+async function getUserByEmail(req, res) {
+    try{
+        const email = req.params.email;
+        const user = await userService.getUserByEmail(email);
+        res.status(200).json(user);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to retrieve user' });
+    }
+}
+
+async function getUserByUsername(req, res) {
+    try{
+        const username = req.params.username;
+        const user = await userService.getUserByUsername(username);
+        res.status(200).json(user);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to retrieve user' });
+    }
+}
+
+async function updateUser(req, res) {
+    try{
+        const user = req.body;
+        const user_email = req.params.email;
+        await userService.updateUser(user, user_email);
+        res.status(201).json({ message: 'User updated successfully' });
+    }catch(error){
+        res.status(500).json({ error: 'Failed to update user' });
+    }
+}
+
+async function myProfile(req, res) {
+    try{
+        email = req.userMail;
+        const user = await userService.getUserByEmail(email);
+        
+        const likedevents_obj = await userService.getLikedEvents(email);
+        let likedevents_ids = [];
+        for(let i = 0; i < likedevents_obj.length; i++){
+            likedevents_ids.push(likedevents_obj[i].event_id);
+        }
+
+        let likedevents = [];
+        for(let i = 0; i < likedevents_ids.length; i++){
+            likedevents.push(await eventService.getEventById(likedevents_ids[i]));
+        }
+        
+        res.status(200).json({user: user, likedevents: likedevents});
+    }catch(error){
+        res.status(500).json({ error: 'Failed to retrieve user' });
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserByEmail,
+    getUserByUsername,
+    updateUser,
+    myProfile
+};
+
--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const UserLikeEvent = require("../models/UserLikeEvent");
 
 async function getAllUsers(filter = {}) {
     return User.find(filter).exec();
@@ -12,8 +13,27 @@ async function getUserByUsername(username) {
     return User.findOne({username}).exec();
 }
 
+async function updateUser(user, user_email) {
+    return User.updateOne({email: user_email}, {
+        email: user_email,
+        password_hash: user.password_hash,
+        username: user.username,
+        role_id: user.role_id,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        birthday: user.birthday
+    });
+}
+
+async function getLikedEvents(user_mail) {
+    return UserLikeEvent.find({user_email: user_mail}).exec();
+}
+
+
 module.exports = {
     getAllUsers,
     getUserByEmail,
     getUserByUsername,
-}
+    updateUser,
+    getLikedEvents
+}; 
