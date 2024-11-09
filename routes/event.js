b07b859fd4fc21817ae3affb5a156609cbec77c1
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2024-11-09 12:39:21 +0100

    feat: user and event routes

--- routes/event.js
+++ routes/event.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const eventController = require("../controllers/eventController");
+const userlikeeventController = require("../controllers/userlikeeventController");
 const upload = require("../middleware/uploadMiddleware");
+const {verifyToken, verifyUserIsEventCreator} = require("../middleware/authMiddleware");
 
 router.get('', eventController.getAllEvents);
 
-router.post('/new', eventController.addEvent);
+router.post('/new', verifyToken, eventController.addEvent);
 
-router.put('/update/:event_id', eventController.updateEvent);
+router.put('/update/:event_id', verifyUserIsEventCreator, eventController.updateEvent);
 
-router.delete('/delete/:event_id', eventController.deleteEvent);
+router.delete('/delete/:event_id', verifyUserIsEventCreator, eventController.deleteEvent);
 
-router.get('/:event_id', eventController.getEventById);
+router.get('/:event_id', verifyToken, eventController.getEventById);
 
-router.get('/:event_id/send_image', (req, res) => {res.render('send_image.pug')});
+router.get('/:event_id/send_image', verifyUserIsEventCreator, (req, res) => {res.render('send_image.pug')});
 
-router.post('/:event_id/send_image', upload.single('image'), eventController.uploadEventImage);
+router.post('/:event_id/send_image', verifyUserIsEventCreator, upload.single('image'), eventController.uploadEventImage);
 
-router.get('/:event_id/get_image', eventController.getImage);
+router.get('/:event_id/get_image', verifyToken, eventController.getImage);
+
+router.post('/:event_id/like', verifyToken, userlikeeventController.likeEvent);
+
+router.post('/:event_id/unlike', verifyToken, userlikeeventController.unlikeEvent);
+
+router.get('/:event_id/likes', verifyToken, userlikeeventController.getUsersWhoLikedEvent);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,6 @@ const upload = require("../middleware/uploadMiddleware");
 
 router.get('/myprofile', userController.myProfile);
 router.put('/update', userController.updateUser);
-router.get('/email', userController.getUserByEmail);
 router.get('/send_image', (req, res) => {res.render('send_image.pug')});
 router.post('/send_image', upload.single('image'), userController.sendImage);
 router.get('/:user_id/get_image', userController.getImage);
