413b2a29050ea6dd1f56aeb0f5ddb4a6689ff410
Valentin H <84991031+Vazelek@users.noreply.github.com>
2025-02-13 21:43:46 +0100

    Add more event filters (#32)
    * Add filter event by theme
    
    * update tsdoc

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -12,9 +12,10 @@ const fs = require('fs');
  * @param name name filter (partial match, case-insensitive)
  * @param price_min minimum price filter
  * @param price_max maximum price filter
+ * @param theme_code theme code filter
  * @returns the query filter object
  */
-function getEventsFilters(creator_email = null, date = null, name = null, price_min = null, price_max = null) {
+function getEventsFilters(creator_email = null, date = null, name = null, price_min = null, price_max = null, theme_code = null) {
     const filter = {}
 
     if (creator_email) {
@@ -22,6 +23,9 @@ function getEventsFilters(creator_email = null, date = null, name = null, price_
     }
     if (date) {
         filter["date"] = date;
+    }
+    if (theme_code) {
+        filter["theme_code"] = theme_code;
     }
     if (name) {
         filter["name"] = {"$regex": name, "$options": "i"};
@@ -58,9 +62,10 @@ async function getAllEvents(req, res) {
             name,
             price_min,
             price_max,
+            theme_code,
         } = req.query;
 
-        const filter = getEventsFilters(creator_email, date, name, price_min, price_max);
+        const filter = getEventsFilters(creator_email, date, name, price_min, price_max, theme_code);
         let events;
 
         if (sort_by_date === '1' || sort_by_date === '-1') {
